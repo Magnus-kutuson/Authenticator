39cad2084da95e4b3ea020516bd201d84e6e3213
Magnus <bcict20070@ttu.edu.gh>
2025-04-10 19:58:16 +0000

    feat: implemented Route in components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path    : 'login',
+        loadComponent: () => import('./core/pages/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path    : 'signup',
+        loadComponent: () => import('./core/pages/signup/signup.component').then(m => m.SignupComponent)
+    }
+
+];
