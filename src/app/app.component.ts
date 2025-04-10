017d3211825ce896973456999fbaeb4c9adf58cf
Magnus <bcict20070@ttu.edu.gh>
2025-04-10 20:04:07 +0000

    Refactor: made changes components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './core/pages/login/login.component';
-import { SignupComponent } from './core/pages/signup/signup.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, SignupComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
