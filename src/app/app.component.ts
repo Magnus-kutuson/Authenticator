17e40d1d2a56cd0cd90f09caeea78ebe88ea2aa0
Magnus <bcict20070@ttu.edu.gh>
2025-04-10 19:46:02 +0000

    feat: designed interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './core/pages/login/login.component';
+import { SignupComponent } from './core/pages/signup/signup.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
