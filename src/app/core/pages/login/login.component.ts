60006b8b98def35fc62b81044c68805d7d5e6d7a
Magnus <bcict20070@ttu.edu.gh>
2025-04-11 09:18:20 +0000

    fix: implemented reactive forms

--- src/app/core/pages/login/login.component.ts
+++ src/app/core/pages/login/login.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
-  imports: [ RouterLink ],
+  imports: [ RouterLink, ReactiveFormsModule, CommonModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      rememberMe: [false]
+    });
+  }
 
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted:', this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched(); // Mark all fields as touched to show validation errors
+    }
+  }
 }
